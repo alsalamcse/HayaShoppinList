ec0dd5e056184f7cc70b7b57738b7f330a163fce
user <hayam.mobarky@gmailcom>
2017-10-04 13:25:25 +0300

    haya

--- app/src/main/java/com/example/user/hayashoppinlist/LogInActivity.java
+++ app/src/main/java/com/example/user/hayashoppinlist/LogInActivity.java
@@ -2,12 +2,33 @@ package com.example.user.hayashoppinlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private EditText etPassword;
+    private  EditText etName;
+    private Button btSingup;
+    private Button btSingin;
+    private Button btForget;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        etPassword =(EditText) findViewById(R.id.etPassword);
+        etName =(EditText) findViewById(R.id.etName);
+        btSingup =(Button) findViewById(R.id.btSingup);
+        btSingin =(Button) findViewById(R.id.btSingin);
+        btForget =(Button) findViewById(R.id.btForget);
+
+
+
+
+
     }
 }
